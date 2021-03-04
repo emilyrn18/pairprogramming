65d447b41ee33b781c72c552df94dbe592c4c387
emilyrn18 <emilyrn18@vt.edu>
2021-03-04 11:50:14 -0500

    added button and list functionality

--- app/src/main/java/com/example/pairprogramming/MainActivity.java
+++ app/src/main/java/com/example/pairprogramming/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.pairprogramming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button rec;
+    TextView list;
+    String times;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rec = (Button) findViewById(R.id.record);
+        list = (TextView) findViewById(R.id.timelist);
+        times = "";
+    }
+
+    public void recordAction(View view){
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        times += currentTime + "\n";
+        list.setText(times);
     }
 }
